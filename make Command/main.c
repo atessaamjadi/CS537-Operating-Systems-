58f506d9dd2c697ed361b14a3edd701dd4240b54
atessaamjadi <74693345+atessaamjadi@users.noreply.github.com>
2021-02-10 21:24:32 -0600

    Update main.c

--- make Command/main.c
+++ make Command/main.c
@@ -1,3 +1,25 @@
+////////////////////////////////////////////////////////////////////////////////
+// Main File:        (main.c)
+// This File:        (main.c)
+// Other Files:      (graph.h, graph.c, process.c, process.h, parser.c, parser.h)
+// Semester:         CS 537 Fall 2020
+//
+// Author:           (Atessa Amjadi, Katie Telford)
+// NetID:            (aamjadi, ktelford)
+// CS Login:         (atessa, telford)
+//
+/////////////////////////// OTHER SOURCES OF HELP //////////////////////////////
+//                   Fully acknowledge and credit all sources of help,
+//                   other than Instructors and TAs.
+//
+// Persons:          Identify persons by name, relationship to you, and email.
+//                   Describe in detail the the ideas and help they provided.
+//
+// Online sources:   Avoid web searches to solve your problems, but if you do
+//                   search, be sure to include Web URLs and description of
+//                   of any information you find.
+/////////////////////////////////////////////////////////////////////////////////
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
